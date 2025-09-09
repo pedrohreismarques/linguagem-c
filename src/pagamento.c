4e8adf1edbbc2d57f6b3a1dddb7994e317900fef
Pedro Henrique <pedrohreismarques@gmail.com>
2025-09-09 16:50:45 -0300

    fix: correção da entrada de dados para a forma de pagamento

--- src/pagamento.c
+++ src/pagamento.c
@@ -1,32 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(){
+int main()
+{
     system("clear");
+    
     float preco, resultado;
     char forma_pg;
-    printf("digite o valor a ser pago e tecle enter\n");
-    scanf("%f",&preco);
-    printf("digite a forma de pagamento, sendo:\n c-> crédito\n d-> debito\nD-> dinheiro\np-> pix\n");
-    scanf("%c",&forma_pg);
+    
+    printf("Digite o valor a ser pago e tecle Enter\n");
+    scanf("%f", &preco);
 
-    if( forma_pg == 'c'){
+    getchar();
+    
+    printf("digite a forma de pagamento, sendo:\n");
+    printf("c-> crédito\nd-> debito\nD-> dinheiro\np-> pix\n");
+    scanf("%c", &forma_pg);
+
+    if (forma_pg == 'c')
+    {
         resultado = preco * (0.05 + 1);
-        printf("o valor a ser pago no crédito é %f\n",resultado);
+        printf("o valor a ser pago no crédito é %.2f\n", resultado);
     }
-    else if(forma_pg == 'd'){
-        printf("o valor a se pago no debito é %f",preco);
+    else if (forma_pg == 'd')
+    {
+        printf("o valor a se pago no debito é %.2f", preco);
     }
-    else if(forma_pg == 'D'){
+    else if (forma_pg == 'D')
+    {
         resultado = preco * 0.98;
-        printf("o valor a ser pago no dinheiro é %f\n",resultado);
+        printf("o valor a ser pago no dinheiro é %.2f\n", resultado);
     }
-    else if (forma_pg == 'p'){
+    else if (forma_pg == 'p')
+    {
         resultado = preco * 0.96;
-        printf("o valor a ser pago no pix é %f\n",resultado);
+        printf("o valor a ser pago no pix é %.2f\n", resultado);
     }
-    else{
+    else
+    {
         printf("ESSA FORMA DE PAGAMENTO NÃO EXISTE\n");
     }
     return 0;
-    
 }
